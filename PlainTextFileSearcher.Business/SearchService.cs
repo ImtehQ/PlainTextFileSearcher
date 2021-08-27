75cc3acdd9cbaad69039ada9df86561074f1633e
ImtehQ <its_q@hotmail.com>
2021-08-27 16:03:37 +0200

    Add project files.

---
+++ PlainTextFileSearcher.Business/SearchService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PlainTextFileSearcher.Business
+{
+    public class SearchService
+    {
+        public List<string> folderPaths = new List<string>();
+        public List<string> filePaths = new List<string>();
+
+        public void IndexFolders(string startingPath)
+        { 
+            folderPaths.AddRange(FindFolders(startingPath));
+            filePaths.AddRange(FindFiles(startingPath));
+        }
+
+        private string[] FindFolders(string path)
+        {
+            return System.IO.Directory.GetDirectories(path, "*", System.IO.SearchOption.AllDirectories); 
+        }
+        private string[] FindFiles(string path)
+        {
+            return System.IO.Directory.GetFiles(path, "*", System.IO.SearchOption.AllDirectories);
+        }
+
+        public async Task<List<Tuple<int, int>>> FindInAllFiles(string searchWord)
+        {
+            List<Tuple<int, int>> FindInAllFilesResultList = new List<Tuple<int, int>>();
+
+            Parallel.For(0, filePaths.Count,
+            index => 
+            {
+                List <Tuple<int, int>> r = FindInFile(filePaths[index], searchWord).Result;
+                FindInAllFilesResultList.AddRange(r);
+            });
+            return FindInAllFilesResultList;
+        }
+
+        private async Task<List<Tuple<int, int>>> FindInFile(string pathToFile, string searchWord)
+        {
+            List<Tuple<int, int>> resultListTuple = new List<Tuple<int, int>>();
+            string[] lines = System.IO.File.ReadAllLines(pathToFile);
+
+            Parallel.For(0, lines.Length,
+                   index => {
+                       List<int> results = FindAllInString(lines[index], searchWord);
+                       for (int r = 0; r < results.Count; r++)
+                       {
+                           resultListTuple.Add(new Tuple<int, int>(results[r], index));
+                       }
+                   });
+            return resultListTuple;
+        }
+
+        private List<int> FindAllInString(string input, string searchWord)
+        {
+            List<int> results = new List<int>();
+            int nextIndex = 0;
+
+
+            while (nextIndex >= 0 && nextIndex + searchWord.Length <= input.Length)
+            {
+                nextIndex = NextIndexOf(input, searchWord, nextIndex);
+                if (nextIndex >= 0)
+                {
+                    results.Add(nextIndex);
+                    nextIndex += searchWord.Length;
+                }
+            }
+            return results;
+        }
+
+        private int NextIndexOf(string input, string match, int startIndex)
+        {
+            if (startIndex == -1)
+                return -1;
+            var result = input.IndexOf(match, startIndex, match.Length, StringComparison.OrdinalIgnoreCase);
+            return result;
+        }
+    }
+}
---
+++ PlainTextFileSearcher.Winforms/Form1.cs
@@ -0,0 +1,87 @@
+﻿using PlainTextFileSearcher.Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlainTextFileSearcher.Winforms
+{
+    public partial class Form1 : Form
+    {
+        private const string SEARCH = "Search";
+        private const string CANCEL = "Cancel";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Tuple<int, int, string>> searchResults = new List<Tuple<int, int, string>>();
+        private string TextBoxResult = "";
+        private Stopwatch sw = new Stopwatch();
+
+        private async void btnSearch_ClickAsync(object sender, EventArgs e)
+        {
+            if (btnSearch.Text == SEARCH)
+            {
+                btnSearch.Text = CANCEL;
+                SearchService ss = new SearchService();
+                //===============================================================================
+
+                sw.Start();
+                ss.IndexFolders(lblOpenedFolder.Text);
+                sw.Stop();
+                Debug.WriteLine($"IndexFolders took: {sw.ElapsedMilliseconds} ms");
+                sw.Reset();
+
+                //===============================================================================
+
+                sw.Start();
+                var results = await ss.FindInAllFiles(tbxSearch.Text);
+                sw.Stop();
+                Debug.WriteLine($"FindInFile took: {sw.ElapsedMilliseconds} ms, {results.Count} matches found!");
+                sw.Reset();
+
+                //===============================================================================
+
+                sw.Start();
+                
+                tbxSearchResults.Text = TextBoxResult;
+
+                //lblTimePassedMs.Text = $"{ss.sw.ElapsedMilliseconds.ToString()} ms";
+                LblTotaalFolderAndFiles.Text = $"Folders: {ss.folderPaths.Count()} & Files: {ss.filePaths.Count()}"; sw.Stop();
+
+                sw.Stop();
+                Debug.WriteLine($"Preparing results took: {sw.ElapsedMilliseconds} ms");
+                sw.Reset();
+                //===============================================================================
+            }
+            else
+            {
+                btnSearch.Text = SEARCH;
+
+                //TODO cancelation
+            }
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+            lblOpenedFolder.Text = folderBrowserDialog.SelectedPath;
+            btnSearch.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblOpenedFolder.Text = @"D:\Itvitea\articles";
+            btnSearch.Enabled = true;
+        }
+    }
+}
